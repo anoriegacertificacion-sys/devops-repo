58dfd77f03663217184d62d8689404998ed193a5
anoriegacertificacion-sys <anoriega.certificacion@gmail.com>
2026-01-04 08:27:16 +0000

    Final certification push

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from flask import Flask, render_template, request
-
+import os
+from flask import Flask, render_template
 app = Flask(__name__)
-
 @app.route("/")
 def main():
     model = {"title": "Hello Build Trigger."}
-    return render_template('index.html', model=model)
-
+    return render_template("index.html", model=model)
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
