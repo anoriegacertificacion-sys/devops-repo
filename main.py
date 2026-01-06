ea2775d3ca3c332a526d7367a5536ed432da38ec
anoriegacertificacion-sys <estudiante@qwiklabs.net>
2026-01-06 05:02:40 +0000

    Initial Commit

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
+
 @app.route("/")
 def main():
-    model = {"title": "Hello Build Trigger."}
-    return render_template("index.html", model=model)
+    model = {"title": "Hello DevOps Fans."}
+    return render_template('index.html', model=model)
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
